9450a44743dabc77d6be759f2c9a35bce6665a0f
Julian Pienaar <julian29pienaar@gmail.com>
2024-11-10 16:56:58 +0200

    Handle logic when completing quiz

--- src/quiz/quiz.ts
+++ src/quiz/quiz.ts
@@ -15,7 +15,15 @@ export function setDifficulty() {
 
         localStorage.setItem("quizResult", total.toString());
 
-
+        localStorage.setItem("t0", "1");
+        localStorage.setItem("t1", "0");
+        localStorage.setItem("t2", "0");
+        localStorage.setItem("t3", "0");
+        localStorage.setItem("t4", "0");
+        localStorage.setItem("t5", "0");
+        localStorage.setItem("t6", "0");
+        localStorage.setItem("t7", "0");
+        localStorage.setItem("t8", "0");
 
         // questions will be layed out 0 to 8
         // 0-2 beginner
@@ -25,9 +33,13 @@ export function setDifficulty() {
             navigateTo(`index.html?t=${0}`);
         }
         if (total === 2) {
+            localStorage.setItem("t3", "1");
             navigateTo(`index.html?t=${3}`);
+
         }
         if (total === 3) {
+            localStorage.setItem("t3", "1");
+            localStorage.setItem("t6", "1");
             navigateTo(`index.html?t=${6}`);
         }
 
