89c44fdf5a94dcacc03b8b3b869e84bc0cd325f9
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2024-11-12 16:11:02 +0000

    Deploying to gh-pages from @ COS-750-Assignment-3/CodeTales@18cfdbc583657102b6273e6a9063ae7915a065c5 🚀

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,3 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export declare function navigateTo(route: string): void;
