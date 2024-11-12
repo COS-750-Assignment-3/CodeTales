6db8488324eeea20a29ae075bbb2563425a29497
TomBlendSoftware <u21446131@tuks.co.za>
2024-11-12 21:19:43 +0200

    Removed unneccessary blocks

--- src/activities/intermediate/toolbox.ts
+++ src/activities/intermediate/toolbox.ts
@@ -62,10 +62,7 @@ export const toolbox = {
           kind: "block",
           type: "logic_boolean",
         },
-        {
-          kind: "block",
-          type: "logic_null",
-        },
+
         {
           kind: "block",
           type: "logic_ternary",
@@ -169,38 +166,7 @@ export const toolbox = {
             },
           },
         },
-        {
-          kind: "block",
-          type: "math_single",
-          inputs: {
-            NUM: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 9,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "math_trig",
-          inputs: {
-            NUM: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 45,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "math_constant",
-        },
+
         {
           kind: "block",
           type: "math_number_property",
@@ -215,30 +181,7 @@ export const toolbox = {
             },
           },
         },
-        {
-          kind: "block",
-          type: "math_round",
-          fields: {
-            OP: "ROUND",
-          },
-          inputs: {
-            NUM: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 3.1,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "math_on_list",
-          fields: {
-            OP: "SUM",
-          },
-        },
+
         {
           kind: "block",
           type: "math_modulo",
@@ -261,84 +204,6 @@ export const toolbox = {
             },
           },
         },
-        {
-          kind: "block",
-          type: "math_constrain",
-          inputs: {
-            VALUE: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 50,
-                },
-              },
-            },
-            LOW: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 1,
-                },
-              },
-            },
-            HIGH: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 100,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "math_random_int",
-          inputs: {
-            FROM: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 1,
-                },
-              },
-            },
-            TO: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 100,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "math_random_float",
-        },
-        {
-          kind: "block",
-          type: "math_atan2",
-          inputs: {
-            X: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 1,
-                },
-              },
-            },
-            Y: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 1,
-                },
-              },
-            },
-          },
-        },
       ],
     },
     {
@@ -346,10 +211,6 @@ export const toolbox = {
       name: "Text",
       categorystyle: "text_category",
       contents: [
-        {
-          kind: "block",
-          type: "text_print", // new print block
-        },
         {
           kind: "block",
           type: "text",
@@ -372,103 +233,7 @@ export const toolbox = {
             },
           },
         },
-        {
-          kind: "block",
-          type: "text_length",
-          inputs: {
-            VALUE: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "abc",
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_isEmpty",
-          inputs: {
-            VALUE: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "",
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_indexOf",
-          inputs: {
-            VALUE: {
-              block: {
-                type: "variables_get",
-              },
-            },
-            FIND: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "abc",
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_charAt",
-          inputs: {
-            VALUE: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_getSubstring",
-          inputs: {
-            STRING: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_changeCase",
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "abc",
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_trim",
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "abc",
-                },
-              },
-            },
-          },
-        },
+
         {
           kind: "block",
           type: "text_count",
@@ -485,157 +250,9 @@ export const toolbox = {
             },
           },
         },
-        {
-          kind: "block",
-          type: "text_replace",
-          inputs: {
-            FROM: {
-              shadow: {
-                type: "text",
-              },
-            },
-            TO: {
-              shadow: {
-                type: "text",
-              },
-            },
-            TEXT: {
-              shadow: {
-                type: "text",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "text_reverse",
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: "text",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "add_text",
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: "abc",
-                },
-              },
-            },
-          },
-        },
       ],
     },
-    {
-      kind: "category",
-      name: "Lists",
-      categorystyle: "list_category",
-      contents: [
-        {
-          kind: "block",
-          type: "lists_create_with",
-        },
-        {
-          kind: "block",
-          type: "lists_create_with",
-        },
-        {
-          kind: "block",
-          type: "lists_repeat",
-          inputs: {
-            NUM: {
-              shadow: {
-                type: "math_number",
-                fields: {
-                  NUM: 5,
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_length",
-        },
-        {
-          kind: "block",
-          type: "lists_isEmpty",
-        },
-        {
-          kind: "block",
-          type: "lists_indexOf",
-          inputs: {
-            VALUE: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_getIndex",
-          inputs: {
-            VALUE: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_setIndex",
-          inputs: {
-            LIST: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_getSublist",
-          inputs: {
-            LIST: {
-              block: {
-                type: "variables_get",
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_split",
-          inputs: {
-            DELIM: {
-              shadow: {
-                type: "text",
-                fields: {
-                  TEXT: ",",
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: "block",
-          type: "lists_sort",
-        },
-        {
-          kind: "block",
-          type: "lists_reverse",
-        },
-      ],
-    },
+
     {
       kind: "sep",
     },
@@ -644,12 +261,6 @@ export const toolbox = {
       name: "Variables",
       categorystyle: "variable_category",
       custom: "VARIABLE",
-    },
-    {
-      kind: "category",
-      name: "Functions",
-      categorystyle: "procedure_category",
-      custom: "PROCEDURE",
     },
   ],
 };
